6b7a1a4d8e1f3f033abf377561c649186729b420
Samyrai47 <sivolobov.ilya2@gmail.com>
2025-04-18 18:09:51 +0300

    fixing monitoring with tags

--- src/main/java/app/springproject/controller/UsersControllerImpl.java
+++ src/main/java/app/springproject/controller/UsersControllerImpl.java
@@ -34,28 +34,27 @@ import org.springframework.web.bind.annotation.RestController;
     histogram = true)
 public class UsersControllerImpl implements UsersController {
   private final UsersService usersService;
-  private final Counter totalRequests;
-  private final Counter registrationRequests;
-  private final Counter authenticationRequests;
-  private final Counter updateRequests;
-  private final Counter deleteRequests;
+  private final Counter usersRequest;
+  private final MeterRegistry registry;
 
   public UsersControllerImpl(UsersService usersService, MeterRegistry registry) {
     this.usersService = usersService;
-    this.registrationRequests = Counter.builder("user.requests.registration").register(registry);
-    this.authenticationRequests =
-        Counter.builder("user.requests.authentication").register(registry);
-    this.updateRequests = Counter.builder("user.requests.update").register(registry);
-    this.deleteRequests = Counter.builder("user.requests.delete").register(registry);
-    this.totalRequests = Counter.builder("user.requests").register(registry);
+    this.usersRequest = Counter.builder("user.requests").tags("type", "total").register(registry);
+    this.registry = registry;
+  }
+
+  private Counter getUserRequestCounter(String type) {
+    return Counter.builder("user.requests")
+        .description("Number of user requests by type")
+        .tags("type", type)
+        .register(registry);
   }
 
   @Override
   @PostMapping("/signin")
   public ResponseEntity<String> authenticate(@RequestBody User user)
       throws UserNotFoundException, AuthenticationDataMismatchException, JsonProcessingException {
-    authenticationRequests.increment();
-    totalRequests.increment();
+    getUserRequestCounter("authentication").increment();
     usersService.authenticate(user.getEmail(), user.getPassword());
     log.info("Successfully logged in with name {}", user.getEmail());
     return ResponseEntity.ok()
@@ -67,8 +66,7 @@ public class UsersControllerImpl implements UsersController {
   @PostMapping("/signup")
   public ResponseEntity<UserDto> registerUser(@RequestBody User user)
       throws UserAlreadyExistsException, UserNotFoundException, JsonProcessingException {
-    registrationRequests.increment();
-    totalRequests.increment();
+    getUserRequestCounter("registration").increment();
     usersService.registerUser(user);
     return ResponseEntity.status(201)
         .body(new UserDto(user.getEmail(), user.getName(), user.getFiles()));
@@ -78,8 +76,7 @@ public class UsersControllerImpl implements UsersController {
   @PatchMapping("/update")
   public ResponseEntity<UserDto> updateUser(@RequestBody User user)
       throws UserNotFoundException, JsonProcessingException {
-    updateRequests.increment();
-    totalRequests.increment();
+    getUserRequestCounter("update").increment();
     usersService.updateUser(user);
     return ResponseEntity.ok()
         .header("userId", String.valueOf(user.getId()))
@@ -90,8 +87,7 @@ public class UsersControllerImpl implements UsersController {
   @DeleteMapping("/delete/{email}")
   public ResponseEntity<UserDto> deleteUser(@PathVariable String email)
       throws UserNotFoundException, JsonProcessingException {
-    deleteRequests.increment();
-    totalRequests.increment();
+    getUserRequestCounter("deletion").increment();
     User user = usersService.deleteUser(email);
     return ResponseEntity.ok(new UserDto(user.getEmail(), user.getName(), user.getFiles()));
   }
